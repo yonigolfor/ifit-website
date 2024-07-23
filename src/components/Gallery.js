dcd6f06933ca48c4479273c9eee51d926930bacd
Yoni Golfor <yonigolfor@gmail.com>
2024-07-23 13:45:16 +0300

    add loading spinner until image is loaded

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,6 +3,7 @@ import './Gallery.css';
 import mockupBarChart from '../images/mockups/mockup exercise barchart.png'
 import mockupMainFeed from '../images/mockups/mockup mainfeed.png'
 import mockupStopWatch from '../images/mockups/mockup stopwatch2.png'
+import ImageWithLoading from './ImageWithLoading';
 
 
 function Gallery() {
@@ -10,9 +11,13 @@ function Gallery() {
     <section className="gallery">
       <div className='image-container-container'>
         <div className="image-container">
-          <img className='rotate-z' src={mockupMainFeed} alt="App preview 1" />
-          <img className='rotate-z' src={mockupBarChart} alt="App preview 1" />
-          <img className='rotate-z' src={mockupStopWatch} alt="App preview 3" />
+          <ImageWithLoading className='rotate-z' src={mockupMainFeed} alt="App preview 1" />
+          <ImageWithLoading className='rotate-z' src={mockupBarChart} alt="App preview 2" />
+          <ImageWithLoading className='rotate-z' src={mockupStopWatch} alt="App preview 3" />
+       
+          {/* <img className='rotate-z' src={mockupMainFeed} alt="App preview 1" /> */}
+          {/* <img className='rotate-z' src={mockupBarChart} alt="App preview 1" /> */}
+          {/* <img className='rotate-z' src={mockupStopWatch} alt="App preview 3" /> */}
         </div>
       </div>
     </section>
---
+++ src/components/ImageWithLoading.js
@@ -0,0 +1,23 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import React, { useState } from 'react';
+ 
+
+
+const ImageWithLoading = ({ src, alt }) => {
+    const [loading, setLoading] = useState(true);
+  
+    return (
+      <div className="image-container rotate-z">
+        { loading && <CircularProgress /> }
+        { !loading && <img
+          src={src}
+          alt={alt}
+          style={loading ? { display: 'none' } : {}}
+          onLoad={() => setLoading(false)}
+        /> 
+}
+      </div>
+    );
+  };
+
+  export default ImageWithLoading
