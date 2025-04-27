a3b36b60a62631757e2e418e93d3f9afe2d10114
Yoni Golfor <yonigolfor@gmail.com>
2025-04-27 17:50:06 +0300

    add link to privacy policy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
-
 import Header from './components/Header';
 import Description from './components/Description';
 import Gallery from './components/Gallery';
 import ContactForm from './components/ContactForm';
+import PrivacyPolicy from './components/privacyPolicy/PrivacyPolicy';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header />
-      <Gallery />
-      <ContactForm />
-      <Description/>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <Header />
+            <Gallery />
+            <ContactForm />
+            <Description />
+            {/* Footer */}
+            <div style={{ paddingBottom: "2px", backgroundColor: "black" }}>
+              <Link to="/privacy-policy" style={{color: "whitesmoke"}}>Privacy Policy</Link>
+            </div>
+          </div>
+        } />
+        
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/privacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="privacy-policy" style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto', color: "whitesmoke", backgroundColor: 'black', minHeight: "100vh" }}>
+      <h1>Privacy Policy</h1>
+      <p>Last updated: 4.27.2025</p>
+      
+      <p>
+        This Privacy Policy describes our policies and procedures on the collection, use, and disclosure
+        of your information when you use our app.
+      </p>
+
+      <h2>Information Collection and Use</h2>
+      <p>
+        We do not collect, use, store, or share any personal information. Your use of the app is entirely private
+        and no personal data is gathered by us in any form.
+      </p>
+
+      <h2>Changes to This Privacy Policy</h2>
+      <p>
+        We may update our Privacy Policy from time to time. Any changes will be posted on this page.
+        We recommend reviewing this Privacy Policy periodically for any updates.
+      </p>
+    </div>
+  );
+}
